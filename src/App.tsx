72f9e5f5fc736b34deff8e0235bd818c71fae33a
devxyn <johnlawrencehermo@gmail.com>
2025-05-01 13:48:23 +0800

    feat: update App component to include Navbar and Showcase components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import Hero from './components/section/Hero';
+import Navbar from './components/Navbar';
+import Hero from './section/Hero';
+import Showcase from './section/Showcase';
 
 const App = () => {
   return (
     <>
+      <Navbar />
       <Hero />
+      <Showcase />
     </>
   );
 };
