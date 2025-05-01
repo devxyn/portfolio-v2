449a22264f5a03c84bcc4a4800717285987fb420
devxyn <johnlawrencehermo@gmail.com>
2025-05-01 18:39:08 +0800

    feat: integrate Contact, Footer, and Testimonials components into the main App layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import Navbar from './components/Navbar';
+import Contact from './section/Contact';
 import Experience from './section/Experience';
 import FeaturedCards from './section/FeaturedCards';
+import Footer from './section/Footer';
 import Hero from './section/Hero';
 // import LogoSection from './section/LogoSection';
 import Showcase from './section/Showcase';
 import TechStack from './section/TechStack';
+import Testimonials from './section/Testimonials';
 
 const App = () => {
   return (
@@ -16,6 +19,9 @@ const App = () => {
       <FeaturedCards />
       <Experience />
       <TechStack />
+      <Testimonials />
+      <Contact />
+      <Footer />
     </>
   );
 };
