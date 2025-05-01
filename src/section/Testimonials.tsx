42bf9fdb20b95b0e94105ac8149127c9a18279ef
devxyn <johnlawrencehermo@gmail.com>
2025-05-01 18:38:14 +0800

    fix: add key prop to GlowCard component in Testimonials for improved rendering

--- src/section/Testimonials.tsx
+++ src/section/Testimonials.tsx
@@ -10,7 +10,7 @@ const Testimonials = () => {
 
         <div className='lg:columns-3 md:columns-2 columns-1 mt-16'>
           {testimonials.map(({ imgPath, name, mentions, review }, index) => (
-            <GlowCard card={{ review }} index={index}>
+            <GlowCard card={{ review }} index={index} key={index}>
               <div className='flex- items-center gap-3'>
                 <div>
                   <img src={imgPath} alt={name} />
