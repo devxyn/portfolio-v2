c80ddf2c54870094d3dc082d7e1eed8525348499
devxyn <johnlawrencehermo@gmail.com>
2025-05-01 18:45:53 +0800

    style: update Footer layout and social icons alignment in CSS for improved responsiveness

--- src/section/Footer.tsx
+++ src/section/Footer.tsx
@@ -5,9 +5,6 @@ const Footer = () => {
   return (
     <footer className='footer'>
       <div className='footer-container'>
-        <div className='flex flex-col justify-center md:items-start items-center'>
-          <a href='/'>Visit my blog</a>
-        </div>
         <div className='socials'>
           {socialImgs.map((img) => (
             <a className='icon' target='_blank' href={img.url}>
