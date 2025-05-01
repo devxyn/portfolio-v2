8da586b4f946f5a54b700f0af7332e81e87db0cb
devxyn <johnlawrencehermo@gmail.com>
2025-05-01 19:05:23 +0800

    fix: add type annotations to Room component and update ref usage for improved TypeScript compatibility

--- src/components/models/hero-models/Room.tsx
+++ src/components/models/hero-models/Room.tsx
@@ -9,9 +9,10 @@ import { EffectComposer, SelectiveBloom } from '@react-three/postprocessing';
 import { BlendFunction } from 'postprocessing';
 import * as THREE from 'three';
 
-export function Room(props) {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function Room(props: any) {
   const { nodes, materials } = useGLTF('/models/optimized-room.glb');
-  const screensRef = useRef();
+  const screensRef = useRef<THREE.Mesh>(null);
   const matcapTexture = useTexture('/images/textures/mat1.png');
 
   const curtainMaterial = new THREE.MeshPhongMaterial({
@@ -46,45 +47,45 @@ export function Room(props) {
     <group {...props} dispose={null}>
       <EffectComposer>
         <SelectiveBloom
-          selection={screensRef}
+          selection={screensRef.current!}
           intensity={1} // Strength of the bloom
           luminanceThreshold={0.2} // Minimum luminance needed
           luminanceSmoothing={0.9} // Smooth transition
           blendFunction={BlendFunction.ADD} // How it blends
         />
       </EffectComposer>
-      <mesh geometry={nodes._________6_blinn1_0.geometry} material={curtainMaterial} />
-      <mesh geometry={nodes.body1_blinn1_0.geometry} material={bodyMaterial} />
-      <mesh geometry={nodes.cabin_blinn1_0.geometry} material={tableMaterial} />
-      <mesh geometry={nodes.chair_body_blinn1_0.geometry} material={chairMaterial} />
-      <mesh geometry={nodes.comp_blinn1_0.geometry} material={compMaterial} />
-      <mesh ref={screensRef} geometry={nodes.emis_lambert1_0.geometry} material={materials.lambert1} />
-      <mesh geometry={nodes.handls_blinn1_0.geometry} material={materials.blinn1} />
-      <mesh geometry={nodes.keyboard_blinn1_0.geometry} material={materials.blinn1} />
-      <mesh geometry={nodes.kovrik_blinn1_0.geometry} material={materials.blinn1} />
-      <mesh geometry={nodes.lamp_bl_blinn1_0.geometry} material={materials.blinn1} />
-      <mesh geometry={nodes.lamp_white_blinn1_0.geometry} material={materials.blinn1} />
-      <mesh geometry={nodes.miuse_blinn1_0.geometry} material={materials.blinn1} />
-      <mesh geometry={nodes.monitor2_blinn1_0.geometry} material={materials.blinn1} />
-      <mesh geometry={nodes.monitor3_blinn1_0.geometry} material={materials.blinn1} />
-      <mesh geometry={nodes.pCylinder5_blinn1_0.geometry} material={materials.blinn1} />
-      <mesh geometry={nodes.pillows_blinn1_0.geometry} material={pillowMaterial} />
-      <mesh geometry={nodes.polySurface53_blinn1_0.geometry} material={materials.blinn1} />
-      <mesh geometry={nodes.radiator_blinn1_0.geometry} material={radiatorMaterial} />
-      <mesh geometry={nodes.radiator_blinn1_0001.geometry} material={materials.blinn1} />
-      <mesh geometry={nodes.railing_blinn1_0.geometry} material={materials.blinn1} />
-      <mesh geometry={nodes.red_bttns_blinn1_0.geometry} material={materials.blinn1} />
-      <mesh geometry={nodes.red_vac_blinn1_0.geometry} material={materials.blinn1} />
-      <mesh geometry={nodes.stylus_blinn1_0.geometry} material={materials.blinn1} />
-      <mesh geometry={nodes.table_blinn1_0.geometry} material={tableMaterial} />
-      <mesh geometry={nodes.tablet_blinn1_0.geometry} material={materials.blinn1} />
-      <mesh geometry={nodes.triangle_blinn1_0.geometry} material={materials.blinn1} />
-      <mesh geometry={nodes.vac_black_blinn1_0.geometry} material={materials.blinn1} />
-      <mesh geometry={nodes.vacuum1_blinn1_0.geometry} material={materials.blinn1} />
-      <mesh geometry={nodes.vacuumgrey_blinn1_0.geometry} material={materials.blinn1} />
-      <mesh geometry={nodes.vires_blinn1_0.geometry} material={materials.blinn1} />
-      <mesh geometry={nodes.window_blinn1_0.geometry} material={materials.blinn1} />
-      <mesh geometry={nodes.window4_phong1_0.geometry} material={materials.phong1} />
+      <mesh geometry={(nodes._________6_blinn1_0 as THREE.Mesh).geometry} material={curtainMaterial} />
+      <mesh geometry={(nodes.body1_blinn1_0 as THREE.Mesh).geometry} material={bodyMaterial} />
+      <mesh geometry={(nodes.cabin_blinn1_0 as THREE.Mesh).geometry} material={tableMaterial} />
+      <mesh geometry={(nodes.chair_body_blinn1_0 as THREE.Mesh).geometry} material={chairMaterial} />
+      <mesh geometry={(nodes.comp_blinn1_0 as THREE.Mesh).geometry} material={compMaterial} />
+      <mesh ref={screensRef} geometry={(nodes.emis_lambert1_0 as THREE.Mesh).geometry} material={materials.lambert1} />
+      <mesh geometry={(nodes.handls_blinn1_0 as THREE.Mesh).geometry} material={materials.blinn1} />
+      <mesh geometry={(nodes.keyboard_blinn1_0 as THREE.Mesh).geometry} material={materials.blinn1} />
+      <mesh geometry={(nodes.kovrik_blinn1_0 as THREE.Mesh).geometry} material={materials.blinn1} />
+      <mesh geometry={(nodes.lamp_bl_blinn1_0 as THREE.Mesh).geometry} material={materials.blinn1} />
+      <mesh geometry={(nodes.lamp_white_blinn1_0 as THREE.Mesh).geometry} material={materials.blinn1} />
+      <mesh geometry={(nodes.miuse_blinn1_0 as THREE.Mesh).geometry} material={materials.blinn1} />
+      <mesh geometry={(nodes.monitor2_blinn1_0 as THREE.Mesh).geometry} material={materials.blinn1} />
+      <mesh geometry={(nodes.monitor3_blinn1_0 as THREE.Mesh).geometry} material={materials.blinn1} />
+      <mesh geometry={(nodes.pCylinder5_blinn1_0 as THREE.Mesh).geometry} material={materials.blinn1} />
+      <mesh geometry={(nodes.pillows_blinn1_0 as THREE.Mesh).geometry} material={pillowMaterial} />
+      <mesh geometry={(nodes.polySurface53_blinn1_0 as THREE.Mesh).geometry} material={materials.blinn1} />
+      <mesh geometry={(nodes.radiator_blinn1_0 as THREE.Mesh).geometry} material={radiatorMaterial} />
+      <mesh geometry={(nodes.radiator_blinn1_0001 as THREE.Mesh).geometry} material={materials.blinn1} />
+      <mesh geometry={(nodes.railing_blinn1_0 as THREE.Mesh).geometry} material={materials.blinn1} />
+      <mesh geometry={(nodes.red_bttns_blinn1_0 as THREE.Mesh).geometry} material={materials.blinn1} />
+      <mesh geometry={(nodes.red_vac_blinn1_0 as THREE.Mesh).geometry} material={materials.blinn1} />
+      <mesh geometry={(nodes.stylus_blinn1_0 as THREE.Mesh).geometry} material={materials.blinn1} />
+      <mesh geometry={(nodes.table_blinn1_0 as THREE.Mesh).geometry} material={tableMaterial} />
+      <mesh geometry={(nodes.tablet_blinn1_0 as THREE.Mesh).geometry} material={materials.blinn1} />
+      <mesh geometry={(nodes.triangle_blinn1_0 as THREE.Mesh).geometry} material={materials.blinn1} />
+      <mesh geometry={(nodes.vac_black_blinn1_0 as THREE.Mesh).geometry} material={materials.blinn1} />
+      <mesh geometry={(nodes.vacuum1_blinn1_0 as THREE.Mesh).geometry} material={materials.blinn1} />
+      <mesh geometry={(nodes.vacuumgrey_blinn1_0 as THREE.Mesh).geometry} material={materials.blinn1} />
+      <mesh geometry={(nodes.vires_blinn1_0 as THREE.Mesh).geometry} material={materials.blinn1} />
+      <mesh geometry={(nodes.window_blinn1_0 as THREE.Mesh).geometry} material={materials.blinn1} />
+      <mesh geometry={(nodes.window4_phong1_0 as THREE.Mesh).geometry} material={materials.phong1} />
     </group>
   );
 }
