3e8595eb54726b3e65c179caaa17cce0822ef626
devxyn <johnlawrencehermo@gmail.com>
2025-05-06 02:10:30 +0800

    feat: update company names in experience and testimonials sections for accurate representation

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -151,6 +151,7 @@ const expCards = [
     imgPath: "/images/jlabs.png",
     logoPath: "/images/icon-jlabs.png",
     title: "Full Stack Developer",
+    company: "JLabs Innovative Co.",
     date: "July 2024 - Present",
     responsibilities: [
       "Developed scalable full-stack applications boosting performance and user engagement.",
@@ -163,6 +164,7 @@ const expCards = [
     review: "",
     imgPath: "/images/uplift.png",
     logoPath: "/images/icon-uplift.png",
+    company: "Uplift Code Camp",
     title: "Full Stack Developer Student",
     date: "August 2023 - March 2024",
     responsibilities: [
@@ -178,6 +180,7 @@ const expCards = [
     imgPath: "/images/shopify.png",
     logoPath: "/images/icon-shopify.png",
     title: "Shopify Developer",
+    company: "Freelance",
     date: "July 2022 - March 2023",
     responsibilities: [
       "Developed high-converting Shopify themes, driving 150% revenue growth through optimized UX/UI.",
@@ -208,42 +211,42 @@ const testimonials = [
     name: "Esther Howard",
     mentions: "@estherhoward",
     review:
-      "I can’t say enough good things about Adrian. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.",
+      "I can’t say enough good things about Lawrence. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.",
     imgPath: "/images/client1.png",
   },
   {
     name: "Wade Warren",
     mentions: "@wadewarren",
     review:
-      "Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.",
+      "Working with Lawrence was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.",
     imgPath: "/images/client3.png",
   },
   {
     name: "Guy Hawkins",
     mentions: "@guyhawkins",
     review:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Lawrence was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Lawrence's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Lawrence is the ideal partner.",
     imgPath: "/images/client2.png",
   },
   {
     name: "Marvin McKinney",
     mentions: "@marvinmckinney",
     review:
-      "Adrian was a pleasure to work with. He turned our outdated website into a fresh, intuitive platform that’s both modern and easy to navigate. Fantastic work overall.",
+      "Lawrence was a pleasure to work with. He turned our outdated website into a fresh, intuitive platform that’s both modern and easy to navigate. Fantastic work overall.",
     imgPath: "/images/client5.png",
   },
   {
     name: "Floyd Miles",
     mentions: "@floydmiles",
     review:
-      "Adrian’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional!",
+      "Lawrence’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional!",
     imgPath: "/images/client4.png",
   },
   {
     name: "Albert Flores",
     mentions: "@albertflores",
     review:
-      "Adrian was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend and backend dev are top-notch.",
+      "Lawrence was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend and backend dev are top-notch.",
     imgPath: "/images/client6.png",
   },
 ];
