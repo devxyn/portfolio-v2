8b6c142b8376a4b2522856658e95783147e3027a
devxyn <johnlawrencehermo@gmail.com>
2025-05-06 11:07:36 +0800

    fix: comment out unused social media entries in data.ts for cleaner code and update LinkedIn URL for accuracy

--- src/constants/data.ts
+++ src/constants/data.ts
@@ -252,24 +252,24 @@ const testimonials = [
 ];
 
 const socialImgs = [
-  {
-    name: "insta",
-    url: "https://www.instagram.com/",
-    imgPath: "/images/insta.png",
-  },
-  {
-    name: "fb",
-    url: "https://www.facebook.com/",
-    imgPath: "/images/fb.png",
-  },
-  {
-    name: "x",
-    url: "https://www.x.com/",
-    imgPath: "/images/x.png",
-  },
+  // {
+  //   name: "insta",
+  //   url: "https://www.instagram.com/",
+  //   imgPath: "/images/insta.png",
+  // },
+  // {
+  //   name: "fb",
+  //   url: "https://www.facebook.com/",
+  //   imgPath: "/images/fb.png",
+  // },
+  // {
+  //   name: "x",
+  //   url: "https://www.x.com/",
+  //   imgPath: "/images/x.png",
+  // },
   {
     name: "linkedin",
-    url: "https://www.linkedin.com/",
+    url: "https://www.linkedin.com/in/johnlawrencehermo/",
     imgPath: "/images/linkedin.png",
   },
 ];
