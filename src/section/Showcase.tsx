eb88976a07a44d9db3f2e6b42734277ea2b7b465
devxyn <johnlawrencehermo@gmail.com>
2025-05-08 15:54:16 +0800

    feat: add TitleHeader component to Showcase for improved project presentation and organization

--- src/section/Showcase.tsx
+++ src/section/Showcase.tsx
@@ -2,6 +2,7 @@ import { useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
+import TitleHeader from "../components/TitleHeader";
 
 gsap.registerPlugin(ScrollTrigger);
 
@@ -40,58 +41,63 @@ const Showcase = () => {
   }, []);
 
   return (
-    <div id='work' ref={sectionRef} className='app-showcase'>
-      <div className='w-full'>
-        <div className='showcaselayout'>
-          <div ref={rydeRef} className='first-project-wrapper'>
-            <div className='image-wrapper'>
-              <a href='https://app.yearglance.com/' target='_blank' rel='noopener noreferrer'>
-                <img src='/images/yg-portfolio.png' alt='Year Glance App' />
-              </a>
-            </div>
-            <div className='text-content'>
-              <h2>YearGlance - A Unified Calendar Tool that Integrates Google and Microsoft Calendars</h2>
-              <p className='text-white-50 md:text-xl'>
-                An app built with React, Bootstrap, & Laravel for a fast, user-friendly experience.
-              </p>
-            </div>
-          </div>
+    <>
+      <TitleHeader title='Projects That I Worked With' sub='💻 What I Can Do' />
 
-          <div className='project-list-wrapper overflow-hidden'>
-            <div className='project' ref={libraryRef}>
-              <div className='image-wrapper bg-[#fbe4ff]'>
-                <a
-                  href='https://play.google.com/store/apps/details?id=com.appzaloot&hl=en'
-                  target='_blank'
-                  rel='noopener noreferrer'>
-                  <img src='/images/az-portfolio.png' alt='App Zaloot Mobile' />
+      <div id='work' ref={sectionRef} className='app-showcase'>
+        <div className='w-full'>
+          <div className='showcaselayout'>
+            <div ref={rydeRef} className='first-project-wrapper'>
+              <div className='image-wrapper bg-[#f3d1cc]'>
+                <a href='https://app.yearglance.com/' target='_blank' rel='noopener noreferrer'>
+                  <img src='/images/yg-portfolio.png' alt='Year Glance App' />
                 </a>
               </div>
               <div className='text-content'>
-                <h2>AppZaloot - A Geo-Location Based Social Media App</h2>
+                <h2>YearGlance - A Unified Calendar Tool that Integrates Google and Microsoft Calendars</h2>
                 <p className='text-white-50 md:text-xl'>
-                  A React Native & Laravel application for seamless social networking on the go.
+                  An app built with React, Bootstrap, & Laravel for a fast, user-friendly experience.
                 </p>
               </div>
             </div>
 
-            <div className='project' ref={ycDirectoryRef}>
-              <div className='image-wrapper bg-[#eafff2]'>
-                <a href='https://bukidmart.onrender.com/' target='_blank' rel='noopener noreferrer'>
-                  <img src='/images/bukidmart-portfolio.png' alt='Bukidmart' />
-                </a>
+            <div className='project-list-wrapper overflow-hidden'>
+              <div className='project' ref={libraryRef}>
+                <div className='image-wrapper bg-[#fbe4ff]'>
+                  <a
+                    href='https://play.google.com/store/apps/details?id=com.appzaloot&hl=en'
+                    target='_blank'
+                    rel='noopener noreferrer'>
+                    <img src='/images/az-portfolio.png' alt='App Zaloot Mobile' />
+                  </a>
+                </div>
+                <div className='text-content'>
+                  <h2>AppZaloot - A Geo-Location Based Social Media App</h2>
+                  <p className='text-white-50 md:text-xl'>
+                    A React Native & Laravel application for seamless social networking on the go.
+                  </p>
+                </div>
               </div>
-              <div className='text-content'>
-                <h2>Bukidmart - A Local Produce E-Commerce App</h2>
-                <p className='text-white-50 md:text-xl'>
-                  A web app built with React, Tailwind, DaisyUI, & Node.js for connecting local farmers with consumers.
-                </p>
+
+              <div className='project' ref={ycDirectoryRef}>
+                <div className='image-wrapper bg-[#eafff2]'>
+                  <a href='https://bukidmart.onrender.com/' target='_blank' rel='noopener noreferrer'>
+                    <img src='/images/bukidmart-portfolio.png' alt='Bukidmart' />
+                  </a>
+                </div>
+                <div className='text-content'>
+                  <h2>Bukidmart - A Local Produce E-Commerce App</h2>
+                  <p className='text-white-50 md:text-xl'>
+                    A web app built with React, Tailwind, DaisyUI, & Node.js for connecting local farmers with
+                    consumers.
+                  </p>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
